919daeab4b3e9679f9fe0a6c7571c794d262365b
Lucas <lucas@192.168.1.9>
2022-12-28 16:22:22 -0300

    Modificado a estrutura do lambda dentro do sort() do método maiorLanceDadoNoLeilao.

--- src/main/java/br/com/alura/leilao/service/FinalizarLeilaoService.java
+++ src/main/java/br/com/alura/leilao/service/FinalizarLeilaoService.java
@@ -31,9 +31,9 @@ public class FinalizarLeilaoService {
 
 	private Lance maiorLanceDadoNoLeilao(Leilao leilao) {
 		List<Lance> lancesDoLeilao = new ArrayList<>(leilao.getLances());
-		lancesDoLeilao.sort((lance1, lance2) -> {
-			return lance2.getValor().compareTo(lance1.getValor());
-		});
+		lancesDoLeilao.sort((lance1, lance2) ->
+			lance2.getValor().compareTo(lance1.getValor())
+		);
 		return lancesDoLeilao.get(0);
 	}
 	
